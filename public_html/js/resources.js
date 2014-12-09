234aa6b96a26259856324051dd2d8373b4fc0e16
Jackson McGlasson <originalqwerty45@gmail.com>
2014-12-09 09:13:39 -0800

    more comments and formatting - Final Commit

---
+++ public_html/js/resources.js
@@ -0,0 +1,37 @@
+//resources the game uses
+game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    {name: "mario", type: "image", src: "data/img/player1.png"},
+    //  {name: "mario", type:"image", src: "data/img/player2.png"},
+    {name: "title-screen", type: "image", src: "data/img/title-screen.png"},
+    {name: "slime", type: "image", src: "data/img/slime-spritesheet.png"},
+    {name: "mushroom", type: "image", src: "data/img/mushroom2.png"},
+    {name: "star", type: "image", src: "data/img/star.png"},
+    /* Atlases  
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
+
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "JLevel01", type: "tmx", src: "data/map/JLevel01.tmx"},
+    {name: "JLevel02", type: "tmx", src: "data/map/JLevel02.tmx"},
+    {name: "JLevel03", type: "tmx", src: "data/map/JLevel03.tmx"}
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
+];
